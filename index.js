25c67bb3360ae2f97fe05ed322f49c6ca13e414a
Andrey Yudin <dj402and@gmail.com>
2017-07-29 01:56:56 +0300

    Add command modules

--- index.js
+++ index.js
@@ -3,13 +3,32 @@ const client = new Discord.Client();
 const fs = require('fs');
 const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
 
+const log = (message) => {
+    var date = new Date();
+    var time = `${date.getHours()}:${date.getMinutes()}`
+    console.log(`${time} [${message.guild.name}][${message.channel.name}] ${message.author.tag}: ${message.content}`);
+}
+
 client.on('ready', () => {
     console.log("<ready>")
 });
 
 client.on('message', (message) => {
     if (message.author.bot || !message.content.startsWith(config.prefix)) return;
-    if (message.content.startsWith(config.prefix+"ping")) message.reply("pong");
+
+    const args = message.content.split(" ");
+    const command = args.shift().slice(config.prefix.length).toLocaleLowerCase();
+
+    log(message);
+
+    try {
+        let commandFile = require(`./commands/${command}.js`);
+        commandFile.run(client, message, args)
+        console.log(`<${command}>`);
+    } catch (error) {
+        console.error(error);
+        message.reply("No such command!");
+    }
 });
 
 client.login(config.token);
